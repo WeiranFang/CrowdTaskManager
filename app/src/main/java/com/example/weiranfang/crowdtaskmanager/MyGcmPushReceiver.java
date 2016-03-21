2a4ef65957c3d044fe5af81b65e4d1be44cbe8f8
WeiranFang <weiran.fang@hotmail.com>
2016-03-21 15:12:14 -0400

    Add GcmPushReceiver.java for showing notification

---
+++ app/src/main/java/com/example/weiranfang/crowdtaskmanager/MyGcmPushReceiver.java
@@ -0,0 +1,107 @@
+package com.example.weiranfang.crowdtaskmanager;
+
+/**
+ * Created by weiranfang on 3/18/16.
+ */
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+public class MyGcmPushReceiver extends GcmListenerService {
+
+    private static final String TAG = MyGcmPushReceiver.class.getSimpleName();
+
+//    private NotificationUtils notificationUtils;
+
+    /**
+     * Called when message is received.
+     *
+     * @param from   SenderID of the sender.
+     * @param bundle Data bundle containing message data as key/value pairs.
+     *               For Set of keys use data.keySet().
+     */
+
+    @Override
+    public void onMessageReceived(String from, Bundle bundle) {
+//        String title = bundle.getString("title");
+        String message = bundle.getString("message");
+//        String image = bundle.getString("image");
+//        String timestamp = bundle.getString("created_at");
+//        Log.e(TAG, "From: " + from);
+//        Log.e(TAG, "Title: " + title);
+//        Log.e(TAG, "message: " + message);
+//        Log.e(TAG, "image: " + image);
+//        Log.e(TAG, "timestamp: " + timestamp);
+//
+//        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+//
+//            // app is in foreground, broadcast the push message
+//            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+//            pushNotification.putExtra("message", message);
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+//
+//            // play notification sound
+//            NotificationUtils notificationUtils = new NotificationUtils();
+//            notificationUtils.playNotificationSound();
+//        } else {
+//
+//            Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+//            resultIntent.putExtra("message", message);
+//
+//            if (TextUtils.isEmpty(image)) {
+//                showNotificationMessage(getApplicationContext(), title, message, timestamp, resultIntent);
+//            } else {
+//                showNotificationMessageWithBigImage(getApplicationContext(), title, message, timestamp, resultIntent, image);
+//            }
+//        }
+
+        showNotificationMessage(message);
+    }
+
+//    /**
+//     * Showing notification with text only
+//     */
+//    private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
+//        notificationUtils = new NotificationUtils(context);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
+//    }
+//
+//    /**
+//     * Showing notification with text and image
+//     */
+//    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+//        notificationUtils = new NotificationUtils(context);
+//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
+//    }
+
+    private void showNotificationMessage(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                .setContentTitle("Message From CrowdTaskManager")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
